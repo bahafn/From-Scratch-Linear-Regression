b86af481b00fc034589f5861f7037a7c40c27128
bahafn <babaalkhateeb@gmail.com>
2026-08-16 00:49:13 +0300

    Some clean ups in main.c code

--- main.c
+++ main.c
@@ -3,12 +3,14 @@
 #include "linear_regression.h"
 
 int main() {
-    DataSet *dataset = read_csv_dataset("test_datasets/test_dataset_2.csv", 0);
+    DataSet *dataset = read_csv_dataset("test_datasets/YearPredictionMSD.txt", 0);
     if (!dataset) {
         return 0;
     }
 
     Split_DataSet split_dataset = train_test_split(dataset, 0.2, 42);
+    destroy_dataset(dataset);
+    free(dataset);
 
     // size_t cols[] = { 0, 4, 6, 10, 12, 51, 13, 20, 21, 13 };
     // Min_Max_Scaler_Set scaler_set = min_max_fit(dataset, 3, cols);
@@ -19,19 +21,19 @@ int main() {
     Linear_Regression_Model model = train_model(&split_dataset.train);
     print_model(&model);
 
-    double *y_pred = predict_matrix(&split_dataset.test.feature_matrix, &model);
+    double *y_pred = predict_all(&split_dataset.test.feature_matrix, &model);
     Linear_Regression_Metrics metrics = calculate_linear_regression_metrics(
-        y_pred,
-        split_dataset.test.target_vector,
-        split_dataset.test.feature_matrix.rows
+            y_pred,
+            split_dataset.test.target_vector,
+            split_dataset.test.feature_matrix.rows
     );
 
     print_linear_regression_metrics(&metrics);
 
-    destroy_dataset(dataset);
-    free(dataset);
+    destroy_split_dataset(&split_dataset);
     destroy_model(&model);
     destroy_scaler_set(&scaler_set);
+    free(y_pred);
 
     return 0;
 }
